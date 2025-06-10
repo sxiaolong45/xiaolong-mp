921b07e93cb9f3cd918eba7b9040db55c8ae7e39
sxiaolong45 <74590603+sxiaolong45@users.noreply.github.com>
2025-06-10 14:42:42 +0800

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -2,12 +2,12 @@
 #sh -c '[ "$1" = "run_portainer" ] && (curl -fsSL https://raw.githubusercontent.com/sxiaolong45/xiaolong-mp/refs/heads/main/portainer.sh || wget -qO - https://raw.githubusercontent.com/sxiaolong45/xiaolong-mp/refs/heads/main/portainer.sh) | sh' _ "%N"
 
 docker run -d \
-  -p 9002:9002 \
-  --name portainer_agent1 \
+  -p 9003:9003 \
+  --name portainer_agent3 \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes \
   -v /:/host \
-  portainer/agent:2.21.4
+  portainer/agent:latest
 
 
