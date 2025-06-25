1418705cc0d76cfd184115ce44caa7bbc83ba2fc
sxiaolong45 <74590603+sxiaolong45@users.noreply.github.com>
2025-06-25 13:36:32 +0800

    Update portainer1.sh

--- portainer.sh
+++ portainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sh -c '[ "$1" = "run_portainer" ] && (curl -fsSL https://raw.githubusercontent.com/sxiaolong45/xiaolong-mp/refs/heads/main/portainer.sh || wget -qO - https://raw.githubusercontent.com/sxiaolong45/xiaolong-mp/refs/heads/main/portainer.sh) | sh' _ "%N"
 
-#docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+docker run -d -p 10000:10000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
 
 
